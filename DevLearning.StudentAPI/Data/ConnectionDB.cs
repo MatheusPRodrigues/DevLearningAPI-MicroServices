89fe30a242f9054a821968a9baf2b46135b9738f
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-12-05 15:48:42 -0300

    feat: Transporting content to the microservice

--- DevLearning.StudentAPI/Data/ConnectionDB.cs
+++ DevLearning.StudentAPI/Data/ConnectionDB.cs
@@ -1,6 +1,19 @@
-﻿namespace DevLearning.StudentAPI.Data
+﻿using Microsoft.Data.SqlClient;
+
+namespace DevLearning.StudentAPI.Data
 {
     public class ConnectionDB
     {
+        private readonly string _connectionString;
+
+        public ConnectionDB(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
--- DevLearning.StudentAPI/Services/Interfaces/IStudentService.cs
+++ DevLearning.StudentAPI/Services/Interfaces/IStudentService.cs
@@ -1,6 +1,16 @@
-﻿namespace DevLearning.StudentAPI.Services.Interfaces
+﻿using DevLearning.Models.DTOs.Student;
+
+namespace DevLearning.StudentAPI.Services.Interfaces
 {
     public interface IStudentService
     {
+        public Task CreateStudent(StudentRequestDTO student);
+        public Task InsertStudentCourse(Guid studentId, Guid courseId, StudentRequestInsertCourseDTO studentCourse);
+        public Task<List<StudentResponseDTO>> GetAllStudents();
+        public Task<StudentResponseDTO> GetStudentById(string id);
+        public Task<StudentResponseDTO> GetStudentByDocument(string document);
+        public Task<StudentResponseDTO> GetStudentByEmail(string email);
+        public Task UpdateStudentCourse(Guid studentId, Guid courseId, StudentCourseRequestUpdateDTO studentCourse);
+        public Task UpdateStudent(StudentRequestUpdateDTO student, string id);
     }
 }
