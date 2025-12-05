220c3f7d486cf5c2466609ce76afa27613c813f4
ChrisCKS <christiancks45@gmail.com>
2025-12-05 11:51:14 -0300

    criando as classes

---
+++ DevLearning.CareerAPI/Repository/Interface/ICareerItemRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DevLearning.CareerAPI.Repository.Interface
+{
+    public interface ICareerItemRepository
+    {
+    }
+}
---
+++ DevLearning.CareerAPI/Service/CareerItemService.cs
@@ -0,0 +1,6 @@
+﻿namespace DevLearning.CareerAPI.Service
+{
+    public class CareerItemService
+    {
+    }
+}
