f78b850acc5d4fdecedb9da84004fccb25532d17
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-12-05 11:39:14 -0300

    deleted auto featured items

---
+++ DevLearning.CourseAPI/Controllers/v1/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLearning.CourseAPI.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
